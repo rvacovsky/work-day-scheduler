f20b134b328e4e3903af0f0e82bbf297df9bf590
Roger Vacovsky <rvacovsky@gmail.com>
2022-04-03 16:35:18 -0400

    added logic for changing timeblock colors

--- script.js
+++ script.js
@@ -5,6 +5,42 @@ $("#currentDay").html(currentDate);
 
 
 
+// change color of timeblocks based on time of day
+function timeBlockColor() {
+  
+    // collect time right now
+    var now = moment().hour();
+  
+    
+    $(".time-block").each(function() {
+    // establish timeblock to compare against current hour
+    var timeBlockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log(timeBlockHour, now)
+
+        // color of timeblock if on current hour
+        if (timeBlockHour === now) {
+            $(this).addClass("present");
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+        } 
+        // color of timeblock if future hour
+        else if (timeBlockHour > now) {
+            $(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+        }
+        // color of timeblock if past hour
+        else {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+    })
+    
+};
+
+timeBlockColor();
+
 
 
 
